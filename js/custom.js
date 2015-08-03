efeda9d1b0c0757fb30c109345520c6d68585db7
goldentechie <bendam1207@gmail.com>
2015-08-03 11:12:23 -0700

    Fix JS

--- js/custom.js
+++ js/custom.js
@@ -21,11 +21,11 @@ $.get( "http://www2.shapeshift.io/crowdsales", function( data ) {
 function openPrivModal(){
     "use strict";
     $('#privModal').modal('show');
- }
- function openTermsModal(){
+}
+function openTermsModal(){
     "use strict";
     $('#termsModal').modal('show');
- }
+}
 
 function opentac(){
 swal({
